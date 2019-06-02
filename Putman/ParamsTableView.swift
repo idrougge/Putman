140b86e3604028d145463e534a804815934306e0
Iggy Drougge <idrougge@mac.com>
2019-06-02 17:21:27 +0200

    Use NSTextMovement enum instead of old-style constants

--- Putman/ParamsTableView.swift
+++ Putman/ParamsTableView.swift
@@ -13,32 +13,31 @@ class ParamsTableView: NSTableView {
     
     override func textDidEndEditing(_ notification: Notification) {
         //print(type(of: self), #function, notification)
-        guard let movement = notification.userInfo?["NSTextMovement"] as? Int else {
+        guard let movement = notification.userInfo?["NSTextMovement"] as? NSTextMovement else {
             return assertionFailure("No text movement")
         }
         switch movement {
-        case NSUpTextMovement: print("NSUpTextMovement")
-        case NSDownTextMovement: print("NSDownTextMovement")
-        case NSBacktabTextMovement: print("NSBacktabTextMovement")
-        case NSTabTextMovement: print("NSTabTextMovement")
-        case NSLeftTextMovement: print("NSLeftTextMovement")
-        case NSRightTextMovement: print("NSRightTextMovement")
-        case NSReturnTextMovement: print("NSReturnTextMovement")
-        case NSOtherTextMovement: print("NSOtherTextMovement")
-        case NSIllegalTextMovement: print("NSIllegalTextMovement")
-        default: print("Totally unknown movement: \(movement)")
+        case .up: print("NSUpTextMovement")
+        case .down: print("NSDownTextMovement")
+        case .backtab: print("NSBacktabTextMovement")
+        case .tab: print("NSTabTextMovement")
+        case .left: print("NSLeftTextMovement")
+        case .right: print("NSRightTextMovement")
+        case .return: print("NSReturnTextMovement")
+        case .other: print("NSOtherTextMovement")
+        case .cancel: print("NSCancelTextMovement")
+        @unknown default: print("Totally unknown movement: \(movement)")
         }
         let row = editedRow
         let column = editedColumn
         //print("row:", row, "column:", column)
         super.textDidEndEditing(notification)
-        // TODO: Refactor for use of NSTextMovement (OSX 10.13+)
         switch movement {
-        case NSTabTextMovement:
+        case .tab:
             let nextColumn = (column + 1) % numberOfColumns
             self.selectRowIndexes(IndexSet(integer: row), byExtendingSelection: false)
             self.editColumn(nextColumn, row: row, with: nil, select: true)
-        case NSReturnTextMovement:
+        case .return:
             let nextRow = (row + 1) % numberOfRows
             self.selectRowIndexes(IndexSet(integer: nextRow), byExtendingSelection: false)
             self.editColumn(1, row: nextRow, with: nil, select: true)
